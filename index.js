ec7638a353cdac1f8f8920d20c42bbf27058d5b2
unknown <md.rafiur.rahman.saikat@gmail.com>
2022-11-09 01:28:27 +0600

    Get data by id added

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middle wares
+
 app.use(cors());
 app.use(express.json());
 
@@ -42,23 +43,37 @@ app.get("/", async (req, res) => {
       res.send({ result, LimitData });
 
       console.log("WOW ", LimitData);
-    } 
-    catch (error) {
+    } catch (error) {
       console.error(error);
     }
-  }
-  else{
+  } else {
     try {
       const cursor = Service.find({});
       const result = await cursor.toArray();
       res.send({ result, LimitData });
 
       console.log("WOW ", LimitData);
-    } 
-    catch (error) {
+    } catch (error) {
       console.error(error);
     }
   }
+});
+
+app.get("/services", async (req, res) => {
+
+  const ID = req.query.id;
+
+  
+
+  const query = { _id: ObjectId(ID) };
+
+  const result = await Service.findOne(query);
+
+  res.send({result});
+
+  console.log(ID, query);
+
+
 });
 
 app.listen(port, (req, res) => {
