ca3a76832e23ebe8c60742ee3b8bcc3499b9f7ca
unknown <md.rafiur.rahman.saikat@gmail.com>
2022-11-10 10:59:25 +0600

    New Data Can Added By Cloient

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ObjectId } = require("mongodb");
-
 const app = express();
 const port = process.env.PORT || 5000;
-
 // middle wares
-
 app.use(cors());
 app.use(express.json());
 
 // m56JQsWVfjeVllcW
 // MyPhotography
-
 const uri =
   "mongodb+srv://MyPhotography:m56JQsWVfjeVllcW@cluster0.8thupxf.mongodb.net/?retryWrites=true&w=majority";
-
 // const uri = "mongodb://0.0.0.0:27017/";
-
 const client = new MongoClient(uri);
-
 const DbConnect = async () => {
   try {
     await client.connect();
@@ -28,21 +21,16 @@ const DbConnect = async () => {
     console.error(error);
   }
 };
-
 DbConnect();
-
 const Service = client.db("MyPhotography").collection("PhotographyCategory");
 const Review = client.db("MyPhotography").collection("review");
-
 app.get("/", async (req, res) => {
   const LimitData = +req.query.data;
-
   if (LimitData) {
     try {
       const cursor = Service.find({}).limit(LimitData);
       const result = await cursor.toArray();
       res.send({ result, LimitData });
-
       console.log("WOW ", LimitData);
     } catch (error) {
       console.error(error);
@@ -52,23 +40,17 @@ app.get("/", async (req, res) => {
       const cursor = Service.find({});
       const result = await cursor.toArray();
       res.send({ result, LimitData });
-
       console.log("WOW ", LimitData);
     } catch (error) {
       console.error(error);
     }
   }
 });
-
 app.get("/services", async (req, res) => {
   const ID = req.query.id;
-
   const query = { _id: ObjectId(ID) };
-
   const result = await Service.findOne(query);
-
   res.send({ result });
-
   console.log(ID, query);
 });
 
@@ -79,48 +61,42 @@ app.get("/review", async (req, res) => {
     CatName: ID,
   };
 
-  const options = {
-    
-    
-    
-  };
-  const data =Review.find(query);
-const result= await data.toArray();
+  const options = {};
+  const data = Review.find(query);
+  const result = await data.toArray();
   res.send({ result });
 
   console.log(ID, query);
 });
 
-// app.post("/addreview", async (req, res) => {
-//   const id = req.body;
-//   const review = req.body;
 
-//   console.log(id, review);
-//   try {
-//     const result = await Service.updateOne(
-//       { _id: ObjectId(id) },
-//       { $push: { review: review } }
-//     );
 
-//     if (result.insertedId) {
-//       res.send({
-//         success: true,
-//         message: `Successfully added the review ${req.body.name} with id ${result.insertedId}`,
-//       });
-//     } else {
-//       res.send({
-//         success: false,
-//         error: "Couldn't added the review",
-//       });
-//     }
-//   } catch (error) {
-//     console.log(error.name.bgRed, error.message.bold);
-//     res.send({
-//       success: false,
-//       error: error.message,
-//     });
-//   }
-// });
+
+
+// ,..............................................................POST REQ
+app.post("/addreview", async (req, res) => {
+  try {
+    const result = await Review.insertOne(req.body);
+
+    if (result.insertedId) {
+      res.send({
+        success: true,
+        message: `Successfully Added ${req.body.name} with id ${result.insertedId}`,
+      });
+    } else {
+      res.send({
+        success: false,
+        error: "Couldn't Added",
+      });
+    }
+  } catch (error) {
+    console.log(error.name.bgRed, error.message.bold);
+    res.send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
 
 app.listen(port, (req, res) => {
   console.log("OK server is running", port);
